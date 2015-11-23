a7c7bd3cbf4d8482dd2ff22aab4467449eb3da2a
bronter <gabe@launchbadge.com>
2015-11-22 19:40:13 -0800

    Hash passwords

--- resources/session/session.go
+++ resources/session/session.go
@@ -5,13 +5,30 @@ import (
   http "net/http"
   sq "github.com/Masterminds/squirrel"
   db "Exgo/db"
+  rand "crypto/rand"
+  big "math/big"
+  pbkdf2 "golang.org/x/crypto/pbkdf2"
+  sha256 "crypto/sha256"
 )
 
+// FIXME: If the rand stuff in here fails, it
+// will probably crash the app, there is no
+// error handling here
+func hashPass(password string) ([]byte, int, []byte) {
+  salt := make([]byte, 32)
+  rand.Read(salt)
+  ii, _ := rand.Int(rand.Reader, big.NewInt(16000))
+  iterations := int(ii.Int64())
+  hash := pbkdf2.Key([]byte(password), salt, iterations, 32, sha256.New)
+  return salt, iterations, hash
+}
+
 func createUser(username string, email string, password string, name string) *sql.Rows {
+  passwordSalt, passwordIterations, passwordHash := hashPass(password)
   rows, _ := sq.
     Insert("user").
-    Columns("username", "email", "password", "name").
-    Values(username, email, password, name).
+    Columns("username", "email", "password_salt", "password_iterations", "password_hash", "name").
+    Values(username, email, passwordSalt, passwordIterations, passwordHash, name).
     RunWith(db.Client).
     Query()
 
