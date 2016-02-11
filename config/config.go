efc12575d8418ef47992a7b75d9fa474a0bd528e
bronter <gabe@launchbadge.com>
2016-02-10 23:28:54 -0800

    let linters lint

--- config/config.go
+++ config/config.go
@@ -7,22 +7,22 @@ import "fmt"
 var File *confer.Config
 
 func init() {
-  File = confer.NewConfig()
-  appenv := os.Getenv("EXGO_ENV");
-  configPath := fmt.Sprintf("%s", os.Getenv("GOPATH")) + "/src/github.com/Pholey/Exgo/config/"
+	File = confer.NewConfig()
+	appenv := os.Getenv("EXGO_ENV")
+	configPath := fmt.Sprintf("%s", os.Getenv("GOPATH")) + "/src/github.com/Pholey/Exgo/config/"
 
-  // Default to default configs if no environment is set
-  paths := []string{configPath + "application.yml", configPath + "development.yml"}
+	// Default to default configs if no environment is set
+	paths := []string{configPath + "application.yml", configPath + "development.yml"}
 
-  // $EXGO_ENV should be one of "production", "development" or "test"
-  if (appenv != "") {
-    paths = append(paths, configPath + fmt.Sprintf("%s.yml", appenv))
-  }
+	// $EXGO_ENV should be one of "production", "development" or "test"
+	if appenv != "" {
+		paths = append(paths, configPath+fmt.Sprintf("%s.yml", appenv))
+	}
 
-  // Override other config with local config
-  paths = append(paths, configPath + "local.yml")
+	// Override other config with local config
+	paths = append(paths, configPath+"local.yml")
 
-  if err := File.ReadPaths(paths...); err != nil {
-    panic(err)
-  }
+	if err := File.ReadPaths(paths...); err != nil {
+		panic(err)
+	}
 }
